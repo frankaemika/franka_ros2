0fcb46e0e43ce5303b03074b2da9d0e6c9b244c3
erik_al <albert.eriksson@franka.de>
2023-06-27 10:47:00 +0200

    Add launch_test file for move_to_start_controller. Remove old launch file.
    Remove cpp related test code.

--- franka_bringup/launch/move_to_start_example_controller.launch.py
+++ franka_bringup/launch/move_to_start_example_controller.launch.py
@@ -22,11 +22,11 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    robot_ip_parameter_name = 'robot_ip'
-    load_gripper_parameter_name = 'load_gripper'
-    use_fake_hardware_parameter_name = 'use_fake_hardware'
-    fake_sensor_commands_parameter_name = 'fake_sensor_commands'
-    use_rviz_parameter_name = 'use_rviz'
+    robot_ip_parameter_name = "robot_ip"
+    load_gripper_parameter_name = "load_gripper"
+    use_fake_hardware_parameter_name = "use_fake_hardware"
+    fake_sensor_commands_parameter_name = "fake_sensor_commands"
+    use_rviz_parameter_name = "use_rviz"
 
     robot_ip = LaunchConfiguration(robot_ip_parameter_name)
     load_gripper = LaunchConfiguration(load_gripper_parameter_name)
@@ -34,44 +34,57 @@ def generate_launch_description():
     fake_sensor_commands = LaunchConfiguration(fake_sensor_commands_parameter_name)
     use_rviz = LaunchConfiguration(use_rviz_parameter_name)
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            robot_ip_parameter_name,
-            description='Hostname or IP address of the robot.'),
-        DeclareLaunchArgument(
-            use_rviz_parameter_name,
-            default_value='false',
-            description='Visualize the robot in Rviz'),
-        DeclareLaunchArgument(
-            use_fake_hardware_parameter_name,
-            default_value='false',
-            description='Use fake hardware'),
-        DeclareLaunchArgument(
-            fake_sensor_commands_parameter_name,
-            default_value='false',
-            description="Fake sensor commands. Only valid when '{}' is true".format(
-                use_fake_hardware_parameter_name)),
-        DeclareLaunchArgument(
-            load_gripper_parameter_name,
-            default_value='true',
-            description='Use Franka Gripper as an end-effector, otherwise, the robot is loaded '
-                        'without an end-effector.'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution(
-                [FindPackageShare('franka_bringup'), 'launch', 'franka.launch.py'])]),
-            launch_arguments={robot_ip_parameter_name: robot_ip,
-                              load_gripper_parameter_name: load_gripper,
-                              use_fake_hardware_parameter_name: use_fake_hardware,
-                              fake_sensor_commands_parameter_name: fake_sensor_commands,
-                              use_rviz_parameter_name: use_rviz
-                              }.items(),
-        ),
-
-        Node(
-            package='controller_manager',
-            executable='spawner',
-            arguments=['move_to_start_example_controller'],
-            output='screen',
-        ),
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                robot_ip_parameter_name, description="Hostname or IP address of the robot."
+            ),
+            DeclareLaunchArgument(
+                use_rviz_parameter_name,
+                default_value="false",
+                description="Visualize the robot in Rviz",
+            ),
+            DeclareLaunchArgument(
+                use_fake_hardware_parameter_name,
+                default_value="false",
+                description="Use fake hardware",
+            ),
+            DeclareLaunchArgument(
+                fake_sensor_commands_parameter_name,
+                default_value="false",
+                description="Fake sensor commands. Only valid when '{}' is true".format(
+                    use_fake_hardware_parameter_name
+                ),
+            ),
+            DeclareLaunchArgument(
+                load_gripper_parameter_name,
+                default_value="true",
+                description=(
+                    "Use Franka Gripper as an end-effector, otherwise, the robot is loaded "
+                    "without an end-effector."
+                ),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [FindPackageShare("franka_bringup"), "launch", "franka.launch.py"]
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    robot_ip_parameter_name: robot_ip,
+                    load_gripper_parameter_name: load_gripper,
+                    use_fake_hardware_parameter_name: use_fake_hardware,
+                    fake_sensor_commands_parameter_name: fake_sensor_commands,
+                    use_rviz_parameter_name: use_rviz,
+                }.items(),
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["move_to_start_example_controller"],
+                output="screen",
+            ),
+        ]
+    )
