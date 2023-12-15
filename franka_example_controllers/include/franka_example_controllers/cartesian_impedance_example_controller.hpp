237a9ad9359c8dfacd098d6d13dace706d40d460
Kzernobog <adityakamireddypalli@gmail.com>
2023-12-15 14:27:36 +0000

    [WIP] Clean up declarrations

--- franka_example_controllers/include/franka_example_controllers/cartesian_impedance_example_controller.hpp
+++ franka_example_controllers/include/franka_example_controllers/cartesian_impedance_example_controller.hpp
@@ -7,6 +7,9 @@
 #include <rclcpp/rclcpp.hpp>
 
 #include <franka_semantic_components/franka_cartesian_pose_interface.hpp>
+#include <franka_semantic_components/franka_robot_model.hpp>
+#include <franka_semantic_components/franka_robot_state.hpp>
+#include <franka_msgs/msg/franka_robot_state.hpp>
 
 using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
@@ -60,9 +63,8 @@ private:
     void UpdateJointStates();
     Eigen::Matrix<double, 7, 1> saturateTorqueRate(
         const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
-        const Eigen::Matrix<double, 7, 1>& tau_J_d)
+        const Eigen::Matrix<double, 7, 1>& tau_J_d);
     std::string arm_id_;
-    const int num_joints = 7;
 
     // Interface for Panda model for dynamics and kinematics
     std::unique_ptr<franka_semantic_components::FrankaRobotModel> franka_robot_model_;
@@ -74,13 +76,25 @@ private:
     std::unique_ptr<franka_semantic_components::FrankaRobotState> franka_robot_state_;
 
     // message object that takes in robot state
-    std::unique_ptr<franka_msgs::msg::FrankaRObotState> robot_state_;
+    std::unique_ptr<franka_msgs::msg::FrankaRobotState> robot_state_;
 
     const std::string k_robot_state_interface_name{"robot_state"};
     const std::string k_robot_model_interface_name{"robot_model"};
 
-    const double delta_tau_max_{1.0}
-
+    const double delta_tau_max_{1.0};
+    double nullspace_stiffness_{20.0};
+    double nullspace_stiffness_target_{20.0};
+    int num_joints{7};
+    bool k_elbow_activated{true};
+    Eigen::Matrix<double, 6, 6> cartesian_stiffness_;
+    Eigen::Matrix<double, 6, 6> cartesian_stiffness_target_;
+    Eigen::Matrix<double, 6, 6> cartesian_damping_;
+    Eigen::Matrix<double, 6, 6> cartesian_damping_target_;
+    Eigen::Matrix<double, 7, 1> q_d_nullspace_;
+    Eigen::Vector3d position_d_;
+    Eigen::Quaterniond orientation_d_;
+    Eigen::Vector3d position_d_target_;
+    Eigen::Quaterniond orientation_d_target_;
 
 };
 } // namespace franka_example_controllers
--- franka_example_controllers/src/cartesian_impedance_example_controller.cpp
+++ franka_example_controllers/src/cartesian_impedance_example_controller.cpp
@@ -1,37 +1,54 @@
 #include <cstddef>
 #include <franka_example_controllers/cartesian_impedance_example_controller.hpp>
+#include <franka_example_controllers/pseudo_inversion.hpp>
 #include <cassert>
 #include <cmath>
 #include <exception>
 #include <string>
 #include <Eigen/Eigen>
-#include <std_msgs/msg/header.cpp>
+#include <franka/robot_state.h>
+
+// NOTE: Build Errors and interface questions
+// TODO: Declare robot_state_
+// TODO: franka_robot_model_ not declared
+// TODO: jacobian_array not declared
+// TODO: position_d_ not declared
+// TODO: orientation_d_ not declared
+// TODO: cartesian_stiffness_ not declared
+// TODO: cartesian_damping_ not declared
+// TODO: nullspace_stiffness_ not declared
+// TODO: q_d_nullspace_ not declared
+// TODO: tau_J_d not declared
+// TODO: saturateTorque not declared
+// TODO: num_joints not declared
+// TODO: command_interfaces_ not declared
+// TODO: franka_cartesian_pose_ undeclared
+// TODO: Maybe get ros parameters in on_init(). ref:
+// franka_robot_state_broadcaster
 
 namespace franka_example_controllers {
 
 controller_interface::InterfaceConfiguration
   CartesianImpedanceExampleController::command_interface_configuration() const {
   /*
-   * TODO:
    * Define command interfaces here
   */
   controller_interface::InterfaceConfiguration config;
   config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
 
-  // one way to get command config interfaces
+  // DOUBT: Is there any other way to get command config
   for (int i = 1; i <= num_joints; i++) {
-    config.names.push_back(arm_id_ + "_joint" + std::to_string(i), + "/effort");
+    config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/effort");
   }
 
   // get it from robot state semantic components
-  config.names = franka_robot_state_->get_command_interface_names();
+  // config.names = franka_robot_state_->get_command_interface_names();
   return config;
 }
 
 controller_interface::InterfaceConfiguration
   CartesianImpedanceExampleController::state_interface_configuration() const {
   /*
-   * TODO:
    * Define state interfaces
   */
   controller_interface::InterfaceConfiguration config;
@@ -42,7 +59,10 @@ controller_interface::InterfaceConfiguration
 
   // Creates state interface for everything published in robot state (joint
   // position, velocity)
-  config.names.push_back(franka_robot_state_->get_state_interface_names());
+  // config.names.push_back(franka_robot_state_->get_state_interface_names());
+  for (const auto& franka_robot_state_name : franka_robot_state_->get_state_interface_names()) {
+    config.names.push_back(franka_robot_state_name);
+  }
   // for (int i = 1; i<=num_joints; i++) {
   //   config.names.push_back(arm_id_ + "_joint" + std::to_string(i), + "/position");
   //   config.names.push_back(arm_id_ + "_joint" + std::to_string(i), + "/velocity");
@@ -54,20 +74,21 @@ controller_interface::InterfaceConfiguration
     config.names.push_back(franka_robot_model_name);
   }
 
-  return config
+  return config;
 }
 
-controller_interface::return_type update(
+controller_interface::return_type CartesianImpedanceExampleController::update(
   const rclcpp::Time& time,
   const rclcpp::Duration& period) {
   /*
    * Generates and writes the joint torques to the joints of the robot
-   * TODO:
   */
   // get robot state
   // DOUBT: placeholder for getting complete robot state. Pass a franka state
   // message into this function?
-  robot_state_ = std::make_unique<franka_msgs::msg::FrankaRobotState>();
+  robot_state_ =
+    std::make_unique<franka_msgs::msg::FrankaRobotState>(
+      franka_msgs::msg::FrankaRobotState());
   franka_robot_state_->get_values_as_message(robot_state_);
 
   // get coriolis
@@ -79,7 +100,7 @@ controller_interface::return_type update(
 
   // Convert to eigen
   Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis(coriolis_array.data());
-  Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(jacobian_array.data());
+  Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian(Jacobian_array.data());
   Eigen::Map<Eigen::Matrix<double, 7, 1>> q(robot_state_.q.data());
   Eigen::Map<Eigen::Matrix<double, 7, 1>> dq(robot_state_.dq.data());
   Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_j_d(
@@ -113,11 +134,11 @@ controller_interface::return_type update(
 
   // pseudoinverse for nullspace handling
   Eigen::MatrixXd jacobian_transpose_pinv;
-  pseudoinverse(jacobian.transpose(), jacobian_transpose_pinv);
+  pseudoInverse(jacobian.transpose(), jacobian_transpose_pinv);
 
   // Cartesian PD control with damping ratio = 1
   // TODO: implement cartesian_stiffness and cartesian dampness
-  tau_task << jacobian,transpose() * 
+  tau_task << jacobian.transpose() * 
    (-cartesian_stiffness_ * error - cartesian_damping_ * (jacobian * dq));
   tau_nullspace << (Eigen::MatrixXd::Identity(7, 7) -
     jacobian.transpose()* jacobian_transpose_pinv) *
@@ -126,8 +147,8 @@ controller_interface::return_type update(
 
   tau_d << tau_task + tau_nullspace + coriolis;
 
-  // TODO: implement saturation of torque commanded
-  tau_d << saturateTorqueRate(tau_d, tau_J_d);
+  // saturate the commanded torque to joint limits
+  tau_d << saturateTorqueRate(tau_d, tau_j_d);
 
   // command the torques
   for (int i = 0; i < num_joints; i++) {
@@ -135,31 +156,68 @@ controller_interface::return_type update(
   }
 
   // update parameters
+  // fin
+  return controller_interface::return_type::OK;
 }
 
 CallbackReturn CartesianImpedanceExampleController::on_init() {
   /*
-   * TODO: Intialise some stuff
-   * Intialize variables
+   * Declare variables
    * Reserve memory
    * Declare node parameters
   */
+  try {
+    if (!get_node()->get_parameter("arm_id", arm_id_)) {
+      RCLCPP_FATAL(get_node()->get_logger(), "Failed to get arm_id parameter");
+      get_node()->shutdown();
+      return CallbackReturn::ERROR;
+    }
+    position_d_.setZero();
+    orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
+    position_d_target_.setZero();
+    orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
+
+    cartesian_stiffness_.setZero();
+    cartesian_damping_.setZero();
+  } catch (const std::exception& e) {
+    fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+    return CallbackReturn::ERROR
+  }
+
+
+
+  return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn CartesianImpedanceExampleController::on_configure(
   const rclcpp_lifecycle::State& previous_state) {
   /*
-   * TODO: Intitialise something here as well
-   * This is where you instantiate objects
+   * TODO:
+   * Initialise variables
    * Read parameters here
   */
-  arm_id_ = get_node()
+  franka_cartesian_pose_ = 
+    std::make_unique<franka_semantic_components::FrankaCartesianPoseInterface>(
+    franka_semantic_components::FrankaCartesianPoseInterface(k_elbow_activated));
+
+  franka_robot_state_ =
+    std::make_unique<franka_semantic_components::FrankaRobotState>(
+      franka_semantic_components::FrankaRobotState(arm_id_ + "/" + k_robot_state_interface_name));
+
+  franka_robot_model_ =
+    std::make_unique<franka_semantic_components::FrankaRobotModel>(
+      franka_semantic_components::FrankaRobotModel(
+        arm_id_ + "/" + k_robot_model_interface_name,
+        arm_id_ + "/" + k_robot_state_interface_name));
+  // arm_id_ = get_node()
+
+  RCLCPP_DEBUG(get_node()->get_logger(), "configured successfully");
+  return CallbackReturn::SUCCESS;
 }
 
 CallbackReturn CartesianImpedanceExampleController::on_activate(
   const rclcpp_lifecycle::State& previous_state) {
   /*
-   * TODO:
    * Assign loaned command and state interfaces
   */
   // state interfaces
@@ -176,7 +234,6 @@ CallbackReturn CartesianImpedanceExampleController::on_activate(
 CallbackReturn CartesianImpedanceExampleController::on_deactivate(
   const rclcpp_lifecycle::State& previous_state) {
   /*
-   * TODO:
    * De-assign loaned command and state interfaces
   */
 
@@ -192,7 +249,7 @@ void CartesianImpedanceExampleController::UpdateJointStates() {
 
 }
 
-Eigen::Matrix<double, 7, 1> saturateTorqueRate(
+Eigen::Matrix<double, 7, 1> CartesianImpedanceExampleController::saturateTorqueRate(
     const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
     const Eigen::Matrix<double, 7, 1>& tau_J_d) {
   Eigen::Matrix<double, 7, 1> tau_d_saturated{};
@@ -205,4 +262,7 @@ Eigen::Matrix<double, 7, 1> saturateTorqueRate(
 }
 
 } // namespace franka_example_controllers
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(franka_example_controllers::CartesianImpedanceExampleController,
+                       controller_interface::ControllerInterface)
 
